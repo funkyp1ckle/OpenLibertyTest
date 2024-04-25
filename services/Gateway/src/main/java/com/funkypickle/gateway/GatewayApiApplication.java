82b116bc7a3d5dc5f71dff8c91e0e1966edab9e4
funkypickle <pramit.govindaraj@gmail.com>
2024-04-24 22:16:58 -0400

    Worked on service discovery and routing

--- services/Gateway/src/main/java/com/funkypickle/gateway/GatewayApiApplication.java
+++ services/Gateway/src/main/java/com/funkypickle/gateway/GatewayApiApplication.java
@@ -2,9 +2,22 @@ package com.funkypickle.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
 
 @SpringBootApplication
 public class GatewayApiApplication {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("device", r -> r.path("/device/**")
+						.filters(f -> f.stripPrefix(1))
+						.uri("lb://DEVICE-DATA-SERVICE"))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApiApplication.class, args);
